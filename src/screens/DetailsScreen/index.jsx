064e26bdf883f74377f75a58656fd137ce6bfe79
fabri075 <juanfabri69@hotmail.com>
2023-07-04 14:00:36 -0300

    changes detail

--- src/screens/DetailsScreen/index.jsx
+++ src/screens/DetailsScreen/index.jsx
@@ -1,15 +1,29 @@
-import { View, Text } from 'react-native';
-import React from 'react';
-import { useSelector } from 'react-redux';
+import { View, Text, Image } from "react-native";
+import React from "react";
+import { useSelector } from "react-redux";
+import styles from "./styles";
 
 const DetailsScreen = () => {
-	const publication = useSelector(state => state.publications.selected);
-	return (
-		<View>
-			<Text>{publication.titulo} </Text>
-			<Text>{publication.descripcion} </Text>
-		</View>
-	);
+  const publication = useSelector((state) => state.publications.selected);
+  const parts = publication.fecha_publicacion.split("-");
+  const date = new Date(parts[0], parts[1] - 1, parts[2]);
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+  return (
+    <View style={styles.container}>
+      <View style={styles.card}>
+        <Text style={styles.title}>{publication.titulo} </Text>
+        <View style={styles.boxImage}>
+          <Image style={styles.image} source={{ uri: publication.image }} />
+        </View>
+        <Text style={styles.description}>{publication.descripcion} </Text>
+        <Text style={styles.date}>
+          Publicado el {day}-{month}-{year}
+        </Text>
+      </View>
+    </View>
+  );
 };
 
 export default DetailsScreen;
---
+++ src/screens/DetailsScreen/styles.js
@@ -0,0 +1,47 @@
+import { StyleSheet } from "react-native";
+import colors from "../../constants/colors";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 26,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 10,
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+  },
+  boxImage: {
+    backgroundColor: colors.white,
+    height: 250,
+    width: "100%",
+    shadowColor: colors.black,
+    shadowOffset: { width: 1, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 5,
+    elevation: 10,
+    marginBottom: 10,
+  },
+  card: {
+    width: "90%",
+    justifyContent: "center",
+    backgroundColor: colors.secondary,
+    padding: 20,
+    marginTop: 20,
+  },
+  description: {
+    fontSize: 20,
+  },
+  date: {
+    marginTop: 15,
+    textAlign: "right",
+    fontStyle: "italic",
+  },
+});
+
+export default styles;
